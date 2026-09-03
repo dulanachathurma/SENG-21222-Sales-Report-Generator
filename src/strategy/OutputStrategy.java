45e389dad0f9e4657d48ff5430fac1bff9117e84
NANDITHA W.D.L <lemithnanditha41@gmail.com>
2026-09-03 09:43:04 +0530

    edit file

--- src/strategy/OutputStrategy.java
+++ src/strategy/OutputStrategy.java
@@ -4,13 +4,6 @@ import java.io.IOException;
 
 public interface OutputStrategy {
 
-    /**
-     * Writes the given report content to some destination.
-     *
-     * @param reportContent the already-formatted report text to output
-     * @param destination   for ConsoleOutputStrategy this is ignored;
-     *                      for FileOutputStrategy this is the output file path
-     * @throws IOException if writing fails (e.g. file cannot be created)
-     */
+
     void writeReport(String reportContent, String destination) throws IOException;
 }
